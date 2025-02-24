f3332ae4f8a7521754976991fc51d6fa84af7bba
ah-ugo <ahuekweprinceugo@gmail.com>
2025-02-24 14:57:33 +0100

    digiwallet2

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from routes.auth_routes import router as auth_router
 from routes.banking_routes import router as banking_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 app.include_router(banking_router, prefix="/banking", tags=["Banking"])
